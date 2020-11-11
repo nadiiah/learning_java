10ca20d7dfa9398a70b3da2bd4f6db5d0cf1748b
nadiiah <nhryhor@gmail.com>
2020-11-11 14:49:53 +0200

    - Fix merge issue

--- src/main/java/com/chapter3/ArrayUtils.java
+++ src/main/java/com/chapter3/ArrayUtils.java
@@ -28,40 +28,6 @@ public class ArrayUtils {
         System.out.print("Please enter a size of an array: ");
         int size = in.nextInt();
         int[] myArray = new int[size];
-        System.out.print("Please enter elements of the array: ");
-        for (int i = 0; i < myArray.length; i++) {
-            myArray[i] = in.nextInt();
-        }
-        return myArray;
-    }
-
-    public static void trySorting(int[] array) {
-        System.out.println("Arrays.sort is much easier way to sort an array, Ira! :)");
-        Arrays.sort(array);
-        System.out.print("Sorted array: ");
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + StringUtils.SPACE);
-        }
-        System.out.print(StringUtils.LF);
-    }
-
-    public static void copyArray(int[] array) {
-        int[] smallPrimes = array;
-        int[] luckyNumbers = smallPrimes;
-        luckyNumbers[1] = 12;
-        System.out.printf("Element of original array: %s.\n", smallPrimes[1]);
-        System.out.printf("Element of copied variable: %s.\n", luckyNumbers[1]);
-        int[] copiedLuckyNumbers = Arrays.copyOf(luckyNumbers, luckyNumbers.length);
-        copiedLuckyNumbers[1] = 15;
-        System.out.printf("Element of original array: %s.\n", luckyNumbers[1]);
-        System.out.printf("Element of copied array: %s.\n", copiedLuckyNumbers[1]);
-    }
-
-    public static int[] readArray() {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Please enter a size of an array: ");
-        int size = in.nextInt();
-        int[] myArray = new int[size];
         System.out.print("Please enter elements of the array divided by spaces: ");
         for (int i = 0; i < myArray.length; i++) {
             myArray[i] = in.nextInt();
